75bb4ba7d56e04de1b0867461328bd129b9b118c
NahuelNGomez <nngomez@fi.uba.ar>
2023-11-02 14:06:13 -0300

    adding navbar

---
+++ src/frontend/medalgram/src/components/NavegationBar.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+
+export default function NavegationBar() {
+
+    return (
+        <nav className="w-full bg-dark1 border-gray-200">
+            <div className="flex flex-wrap items-center justify-between mx-auto p-4">
+            <div className="flex flex-nowrap items-center justify-between">
+                <a href="#" className="flex items-center">
+                    <img src="https://i.ibb.co/hCwXjMC/principal.png" className="h-8 mr-3" alt="principal Logo" />
+                </a>
+                <Link href="/sports" className="nav-link link-body-emphasis active text-gray-300 text-xl underline" aria-current="page">Deportes</Link>
+                </div>
+                <div className="flex md:order-1">
+                    <button type="button" data-collapse-toggle="navbar-search" aria-controls="navbar-search" aria-expanded="false" className="md:hidden text-gray-500 hover:bg-dark0 rounded-lg text-sm p-2.5">
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                        </svg>
+                        <span className="sr-only">Search</span>
+                    </button>
+                    <div className="relative hidden md:block">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                            <span className="sr-only">Search icon</span>
+                        </div>
+                        <input type="text" id="search-navbar" className="focus:outline-none block w-full p-2 pl-10 text-sm text-gray-50 rounded-lg bg-dark0" placeholder="Buscar evento" />
+                    </div>
+                    <button data-collapse-toggle="navbar-search" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-dark0" aria-controls="navbar-search" aria-expanded="false">
+                        <span className="sr-only">Open main menu</span>
+                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                        </svg>
+                    </button>
+                </div>
+                <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-search">
+                    <div className="relative mt-3 md:hidden">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg className="w-4 h-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                            </svg>
+                        </div>
+                        <input type="text" id="search-navbar" className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500" placeholder="Buscar evento" />
+                    </div>
+                    <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
+                        <li>
+                            <div className="bg-dark0 rounded-lg p-1">
+                                <Link href="/login" className="nav-link link-body-emphasis px-2 active text-white" aria-current="page">Iniciar Sesión</Link></div>
+                        </li>
+                        <li>
+                            <div className="bg-white rounded-lg p-1">
+                                <Link href="/signup" className="nav-link link-body-emphasis px-2 active text-dark0" aria-current="page">Crear Cuenta</Link></div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
