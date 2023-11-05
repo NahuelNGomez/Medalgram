908ea1da87a5703ba575d7b5a9489e3741449697
Eliana Harriet <eharriet@fi.uba.ar>
2023-11-05 19:43:29 -0300

    Developing `TableManager` class - Improve of `TableManager` class methods - Adding unit tests for `TableManager` class - Adding integration tests for `TableManager` class

--- src/backend/src/main/java/ar/uba/fi/tdd/docker_demo/backend/db_manager/TableManager.java
+++ src/backend/src/main/java/ar/uba/fi/tdd/docker_demo/backend/db_manager/TableManager.java
@@ -1,8 +1,11 @@
 package ar.uba.fi.tdd.docker_demo.backend.db_manager;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 @Component
 public class TableManager {
@@ -14,27 +17,89 @@ public class TableManager {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public boolean createRecord(String tableName, Object... args) {
-        if (isRecordInDb(tableName, args)) return false;
+    public int getRecordCount(String tableName) {
+        String sql = "SELECT COUNT(*) FROM " + tableName;
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class);
+        return count != null ? count : 0;
+    }
 
-        String sql = createInsertSql(tableName, args.length);
-        jdbcTemplate.update(sql, args);
+    public boolean createRecord(String tableName, Map<String, Object> cols) {
+        if (isRecordInDb(tableName, cols)) return false;
+
+        String sql = createInsertSql(tableName, cols);
+
+        try {
+            jdbcTemplate.update(sql, cols.values().toArray());
+        } catch (DataAccessException e) {
+            return false;
+        }
         return true;
     }
 
-    public boolean isRecordInDb(String tableName, Object... args) {
-        String sql = createSelectSql(tableName, args.length);
-        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, args);
+    public boolean deleteRecord(String tableName, Map<String, Object> cols) {
+        if (!isRecordInDb(tableName, cols)) return false;
+
+        String sql = createDeleteSql(tableName, cols);
+        int rowsAffected = jdbcTemplate.update(sql, cols.values().toArray());
+        return rowsAffected > 0;
+    }
+
+    public boolean isRecordInDb(String tableName, Map<String, Object> cols) {
+        String sql = createSelectSql(tableName, cols);
+        Integer count = jdbcTemplate.queryForObject(sql, Integer.class, cols.values().toArray());
         return count != null && count > 0;
     }
 
-    private String createInsertSql(String tableName, int argCount) {
-        return "INSERT INTO " + tableName + " VALUES (" + "?,".repeat(argCount - 1) +
-                "?)";
+    public int getRecordID(String tableName, Map<String, Object> cols) {
+        String sql = createFindSql(tableName, cols);
+        Integer id = jdbcTemplate.queryForObject(sql, Integer.class, cols.values().toArray());
+        return id != null ? id : -1;
     }
 
-    private String createSelectSql(String tableName, int argCount) {
-        return "SELECT COUNT(*) FROM " + tableName + " WHERE " + "col = ? AND ".repeat(argCount - 1) +
-                "col = ?";
+    private String createInsertSql(String tableName, Map<String, Object> cols) {
+        StringBuilder sql = new StringBuilder("INSERT INTO " + tableName + " (");
+        for (String col : cols.keySet()) {
+            sql.append(col).append(", ");
+        }
+        // Remove the last ", "
+        sql.setLength(sql.length() - 2);
+        sql.append(") VALUES (");
+        for (int i = 0; i < cols.size(); i++) {
+            sql.append("?, ");
+        }
+        // Remove the last ", "
+        sql.setLength(sql.length() - 2);
+        sql.append(")");
+        return sql.toString();
+    }
+
+    private String createDeleteSql(String tableName, Map<String, Object> cols) {
+        StringBuilder sql = new StringBuilder("DELETE FROM " + tableName + " WHERE ");
+        for (String col : cols.keySet()) {
+            sql.append(col).append(" = ? AND ");
+        }
+        // Remove the last " AND "
+        sql.setLength(sql.length() - 5);
+        return sql.toString();
+    }
+
+    private String createSelectSql(String tableName, Map<String, Object> cols) {
+        StringBuilder sql = new StringBuilder("SELECT COUNT(*) FROM " + tableName + " WHERE ");
+        for (String col : cols.keySet()) {
+            sql.append(col).append(" = ? AND ");
+        }
+        // Remove the last " AND "
+        sql.setLength(sql.length() - 5);
+        return sql.toString();
+    }
+
+    private String createFindSql(String tableName, Map<String, Object> cols) {
+        StringBuilder sql = new StringBuilder("SELECT id FROM " + tableName + " WHERE ");
+        for (String col : cols.keySet()) {
+            sql.append(col).append(" = ? AND ");
+        }
+        // Remove the last " AND "
+        sql.setLength(sql.length() - 5);
+        return sql.toString();
     }
 }
