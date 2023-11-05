adc8184c431e0609aca52f8e7aa90d19747b6a19
NahuelNGomez <nngomez@fi.uba.ar>
2023-11-05 19:04:42 -0300

    update previewEvent component

--- src/frontend/medalgram/src/components/PreviewEvent.tsx
+++ src/frontend/medalgram/src/components/PreviewEvent.tsx
@@ -1,9 +1,27 @@
+import Link from "next/link";
+
 export default function PreviewEvent(){
 
     return (
         <main className="flex flex-wrap border w-full my-4">
             <div className="border w-1/5 py-2 flex align-content justify-center">Fecha</div>
-            <div className="border bg-dark2 w-4/5 text-white">asdasd</div>
+            <div className="border bg-dark2 w-4/5 text-white grid grid-cols-2">
+                <div className="col-span-1 flex flex-wrap flex-col">
+                    <p>Titulo</p>
+                    <p>Lugar: Lugar X</p>
+                </div>
+                <div className="flex justify-center items-center">
+                <Link href={`/`} className="nav-link link-body-emphasis active" aria-current="page">
+                    <div className="flex flex-wrap justify-center items-center px-8 py-0.5 text-xs text-gray-300 text-xl bg-dark1">Más información
+                        <svg className="pl-3 w-6 text-white" fill="none" viewBox="0 0 24 24"  stroke="currentColor">
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
+                        </svg>
+                    </div>
+                </Link>
+                </div>
+                
+            </div>
+            
             {/* sin terminar */ }
         </main>
     )
