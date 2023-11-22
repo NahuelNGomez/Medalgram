7721b800a1281d8db82d19bdaab772b92caf40ac
NahuelNGomez <nngomez@fi.uba.ar>
2023-11-22 09:51:22 -0300

    fix images

--- src/frontend/medalgram/src/components/Presentation.tsx
+++ src/frontend/medalgram/src/components/Presentation.tsx
@@ -1,20 +1,9 @@
 import Image from 'next/image'
 export default function NavegationBar() {
-
-    const loaderProp =({ src } : any) => {
-        return src;
-    }
-
     return (
         <section className="flex flex-col justify-center items-center bg-dark2/50 w-4/5 my-4 rounded-xl">
-            <div className="flex items-center justify-between w-1/5 align-items justify-content opacity-50">
-                <Image
-                    src="/principal.png"
-                    alt="Descripción de la imagen"
-                    width={300}
-                    height={200}
-                    loader={loaderProp}
-                />
+            <div className="flex items-center justify-center w-1/5 align-items justify-content opacity-50">
+                <img src="https://i.ibb.co/hCwXjMC/principal.png" className="h-[300px] w-[300px]" alt="principal Logo" />
             </div>
             <div>
                 <h3 className='opacity-50 text-xl'>Medalgram - By bugHunters</h3></div>
