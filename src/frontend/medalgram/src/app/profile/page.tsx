d387a29f5344e6402c3de695bba7ef786c216e58
NahuelNGomez <nngomez@fi.uba.ar>
2023-11-22 14:23:15 -0300

    first integration with de backend

--- src/frontend/medalgram/src/app/profile/page.tsx
+++ src/frontend/medalgram/src/app/profile/page.tsx
@@ -7,14 +7,17 @@ import EditProfileModal from "./components/EditProfileModal";
 import { useEffect, useState } from "react";
 import NavegationBarLogged from "@/components/NavegationBarLogged";
 import AddResultModal from "./components/AddResultModal";
+import { useRouter } from "next/navigation";
+import { verifyToken } from "@/objects/mocks/functions";
 
 export default function Sports() {
   // Hacer get del perfil
   const [logged, setLogged] = useState(false);
 
-
   const [modalProfile, setModalProfile] = useState(false);
   const [modalNewResult, setModalNewResult] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
 
   const closeEditProfile = () => {
     setModalProfile(false);
@@ -34,28 +37,47 @@ export default function Sports() {
 
   useEffect(() => {
     if (document === undefined) return;
-    if (document.cookie !== 'token=null' && document.cookie !== '') {
+    if (verifyToken(document.cookie)) {
       setLogged(true);
     }
   }, [])
+
+  useEffect(() => {
+    if (logged === false) {
+      setLoading(false)
+      return
+    };
+    fetch('https://grupo-3.2023.tecnicasdedisenio.com.ar/api/api/runners/' + verifyToken(document.cookie))
+      .then(response => response.json())
+      .catch(error => {
+        router.push("/createProfile")
+        console.log("No hay datos del usuario - debe crearse un perfil")
+      }
+      )
+  }, [logged])
 
   return (
     <main>
       {
         logged === true ? <NavegationBarLogged /> : <NavegationBar />
       }
-      <div className="flex flex-col">
-        <div className="pt-[50px] pl-[130px] text-3xl">
-          Perfil del corredor 0001
-        </div>
-        <div className="grid grid-cols-6 pl-[130px] pr-[30px] gap-[30px]">
-          <div className="col-span-4 grid grid-rows-2 gap-[30px]">
-            <ProfileInfo editProfile={editProfile} closeEditProfile={closeEditProfile} />
-            <LastResults newResult={newResult} closeNewResult={closeNewResult} />
+      {
+        loading === true ? <div>Cargando...</div> : (
+          <div className="flex flex-col">
+            <div className="pt-[50px] pl-[130px] text-3xl">
+              Perfil del corredor 0001
+            </div>
+            <div className="grid grid-cols-6 pl-[130px] pr-[30px] gap-[30px]">
+              <div className="col-span-4 grid grid-rows-2 gap-[30px]">
+                <ProfileInfo editProfile={editProfile} closeEditProfile={closeEditProfile} />
+                <LastResults newResult={newResult} closeNewResult={closeNewResult} />
+              </div>
+              <ConfirmResults />
+            </div>
           </div>
-          <ConfirmResults />
-        </div>
-      </div>
+
+        )
+      }
       {
         modalProfile === true && (
           <EditProfileModal cancelFunction={closeEditProfile} />
@@ -67,6 +89,6 @@ export default function Sports() {
           <AddResultModal cancelFunction={closeNewResult} />
         )
       }
-    </main>
+    </main >
   );
 }
---
+++ src/frontend/medalgram/src/objects/mocks/functions.tsx
@@ -0,0 +1,17 @@
+export function verifyToken(string : String) {
+    
+    const cookies = string.split(';');
+    
+    let token = null
+
+    for (const cookie of cookies) {
+        const [name, value] = cookie.trim().split('=');
+      
+        if (name !== null) {
+            token = value;
+            console.log(token)
+            return token
+        }
+      }
+    return false;
+}
