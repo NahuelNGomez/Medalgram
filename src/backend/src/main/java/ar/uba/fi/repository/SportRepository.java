8006513e7d4dc5752678aeccbb36fbd4b92b698d
Llaauuttyy <ldabbraccio@fi.uba.ar>
2023-11-22 22:46:19 -0300

    add logic to sport endpoint

--- src/backend/src/main/java/ar/uba/fi/repository/SportRepository.java
+++ src/backend/src/main/java/ar/uba/fi/repository/SportRepository.java
@@ -5,11 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
+import java.util.Optional;
 
 @RepositoryRestResource
 public interface SportRepository extends JpaRepository<Sport, Long> {
 
     Sport findSportById(Long id);
+
+    Optional<Sport> findSportByName(String name);
 
     @Override
     List<Sport> findAll();
