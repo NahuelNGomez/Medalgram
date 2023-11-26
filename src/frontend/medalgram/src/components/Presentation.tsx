1a8fc787679590e6cc23d5f2a472ed7591aa95b1
NahuelNGomez <nngomez@fi.uba.ar>
2023-11-26 13:04:16 -0300

    fix presentation

--- src/frontend/medalgram/src/components/Presentation.tsx
+++ src/frontend/medalgram/src/components/Presentation.tsx
@@ -4,13 +4,14 @@ export default function NavegationBar() {
       <div className="flex justify-center items-center py-5 w-[80%]">
           <div className="flex h-full w-full items-center justify-center mx-auto px-8">
             <div className=" flex flex-col items-center w-full justify-center">
-              <section className="flex flex-col justify-start items-center w-full my-4 rounded-xl bg-[url('https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.roadrunnersports.com%2Fblog%2Fwp-content%2Fuploads%2F2021%2F06%2FRunning-at-Night-Group-of-Girls-Running.jpg')] bg-cover">
-              <h1 className="text-3xl sm:text-5xl tracking-widest text-white lg:text-7xl font-bold text-white/70">
+              <section className="relative flex flex-col justify-start items-center w-full my-4 rounded-xl h-[400px]">
+                <img className="absolute object-cover h-[400px] w-full overflow-hidden object-top z-0" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.roadrunnersports.com%2Fblog%2Fwp-content%2Fuploads%2F2021%2F06%2FRunning-at-Night-Group-of-Girls-Running.jpg"></img>
+              <h1 className="text-3xl sm:text-5xl tracking-widest text-white lg:text-7xl font-bold text-white/70 z-10">
                 Medalgram
               </h1>
                 <img
                   src="https://i.ibb.co/hCwXjMC/principal.png"
-                  className="h-[300px] w-[300px] filter brightness-90"
+                  className="h-[300px] w-[300px] filter brightness-90 z-10"
                   alt="principal Logo"
                 />
               </section>
