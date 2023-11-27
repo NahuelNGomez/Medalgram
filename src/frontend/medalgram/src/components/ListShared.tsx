eeae5f05d706062cf0e5d42594970ce8de707e35
NahuelNGomez <nngomez@fi.uba.ar>
2023-11-26 23:26:50 -0300

    fix key nextjs

--- src/frontend/medalgram/src/components/ListShared.tsx
+++ src/frontend/medalgram/src/components/ListShared.tsx
@@ -29,7 +29,6 @@ export default function ListShared() {
         console.log("se quiere compartir el medallero con:" + e)
     }
 
-
     return (
         <section className="w-full h-full col-span-2 flex flex-col items-center bg-dark2/50 rounded-3xl">
             <h2 className="text-xl font-bold pt-8">Puedes compartir el medallero con las siguientes personas:</h2>
@@ -37,7 +36,7 @@ export default function ListShared() {
                 runners ?
                     runners.filter((runner: any) => runner.first !== 'admin').map((runner: any) => {
                         return (
-                            <div className="w-[60%] flex justify-between items-center py-2 px-4 my-1 bg-dark0 rounded-2xl">
+                            <div key="runner.first" className="w-[60%] flex justify-between items-center py-2 px-4 my-1 bg-dark0 rounded-2xl">
                                 <img src={runner.second} alt="profileImage" width={45} className="rounded-full"/>
                                 <strong>@{runner.first}</strong>
                                 <button className="py-1 bg-dark3 rounded-xl text-black text-sm px-1 hover:bg-dark0 hover:text-white transition duration-300 border-4 border-gray-400 hover:border-blue-400 font-bold text-gray-600" onClick={() => handlerShare(runner.first)}>Compartir Medallero</button>
